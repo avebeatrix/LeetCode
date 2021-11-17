20095334e90cc355eb6a93461de8df872adf3df9
Beatrix_Kido <la_katalina_s@mail.ru>
2021-11-17 12:57:31 +0300

    TwoSum

---
+++ leetcode/algorithms/TwoSum.js
@@ -0,0 +1,24 @@
+// Source : https://leetcode.com/problems/two-sum/
+// Author : avebeatrix
+// Date   : 2021-11-17
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function (nums, target) {
+    let result = [];
+    let subtrahendMap = new Map();
+    nums.forEach((num, i) => {
+        subtrahendMap.set(num - target, i);
+    })
+    for (let i = 0; i < nums.length; i++) {
+        if (subtrahendMap.has(-nums[i]) && i !== subtrahendMap.get(-nums[i])) {
+            result.push(i, subtrahendMap.get(-nums[i]));
+            break;
+        }
+    }
+
+    return result;
+};
