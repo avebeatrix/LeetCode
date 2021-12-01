3320376069a143b90e7f0da79be1aa1aadcd913c
Beatrix_Kido <la_katalina_s@mail.ru>
2021-12-01 10:40:38 +0300

    Linked List Cycle

---
+++ leetcode/algorithms/LinkedListCycle.js
@@ -0,0 +1,31 @@
+// Source : https://leetcode.com/problems/linked-list-cycle/
+// Author : avebeatrix
+// Date   : 2021-12-01
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    if (head === null || head.next === null) {
+        return false;
+    }
+
+    let slow = head;
+    let fast = head.next;
+    while (slow != fast) {
+        if (fast === null || fast.next === null) {
+            return false;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return true;
+};
