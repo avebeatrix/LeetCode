2e6629c516df5148c531c61de8c83a89ba31c5ad
Beatrix_Kido <la_katalina_s@mail.ru>
2021-12-11 00:14:59 +0300

    Remove Element

---
+++ leetcode/algorithms/RemoveElement.js
@@ -0,0 +1,18 @@
+// Source : https://leetcode.com/problems/remove-element/submissions/
+// Author : avebeatrix
+// Date   : 2021-12-11
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    let k = 0;
+    nums.forEach((num, i) => {
+        if (num !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    })
+    return k;
+};
