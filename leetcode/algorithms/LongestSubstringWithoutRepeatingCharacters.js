0b178005d2c4f2d1ae76323deaa3ec013238a30a
Beatrix_Kido <la_katalina_s@mail.ru>
2021-12-26 15:02:00 +0300

    Longest Substring Without Repeating Characters

---
+++ leetcode/algorithms/LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,27 @@
+// Source : https://leetcode.com/problems/longest-substring-without-repeating-characters/
+// Author : avebeatrix
+// Date   : 2021-11-26
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let len = s.length;
+    if (!len) return 0;
+
+    let result = 0;
+    let left = right = 0;
+    let map = new Map();
+
+    while (right < len) {
+        map.set(s[right], (map.get(s[right]) ?? 0) + 1);
+        while (map.get(s[right]) > 1) {            
+            map.set(s[left], map.get(s[left]) - 1);
+            left++;
+        }
+        result = Math.max(result, right - left + 1);
+        right++;      
+    }
+
+    return result;
+};
