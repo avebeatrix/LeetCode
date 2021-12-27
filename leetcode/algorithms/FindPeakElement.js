1b49650f467cb8590d520b0b87b097d6fa552cb2
Beatrix_Kido <la_katalina_s@mail.ru>
2021-12-27 12:55:34 +0300

    Find Peak Element

---
+++ leetcode/algorithms/FindPeakElement.js
@@ -0,0 +1,22 @@
+// Source : https://leetcode.com/problems/find-peak-element/
+// Author : avebeatrix
+// Date   : 2021-11-27
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+    let l = 0;
+    let r = nums.length - 1;
+    while (l < r) {
+        let middle = Math.floor((r + l) / 2);
+        if (nums[middle] > nums[middle + 1]) {
+            r = middle;
+        } else {
+            l = middle + 1;
+        }       
+    }  
+    return l; 
+};
+let d = findPeakElement([1, 2, 3, 1]);
+let k;
