f9d085a377f3a2de955a53581cb8098c89189be6
Beatrix_Kido <la_katalina_s@mail.ru>
2022-01-19 11:06:18 +0300

    Linked List Cycle II

---
+++ leetcode/algorithms/LinkedListCycleII.js
@@ -0,0 +1,33 @@
+// Source : https://leetcode.com/problems/linked-list-cycle-ii/
+// Author : avebeatrix
+// Date   : 2022-01-19
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+    if (!head || !head.next) return null;
+    let slow = head;
+    let fast = head;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next
+        if (slow === fast) {
+            slow = head;
+            while (slow != fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+    }
+    return null;
+};
