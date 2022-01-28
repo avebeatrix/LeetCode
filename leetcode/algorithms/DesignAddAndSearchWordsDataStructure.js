a137a5af1eb11d08cb2eccf3020deedd795211e7
Beatrix_Kido <la_katalina_s@mail.ru>
2022-01-28 11:29:23 +0300

    Design Add and Search Words Data Structure

--- leetcode/algorithms/DesignAddAndSearchWordsDataStructure.js
+++ leetcode/algorithms/DesignAddAndSearchWordsDataStructure.js
@@ -1,6 +1,29 @@
+// Source : https://leetcode.com/problems/design-add-and-search-words-data-structure/
+// Author : avebeatrix
+// Date   : 2022-01-28
+class TrieNode {
+
+    constructor(){
+        this.links = [];  
+        this.isEnd = false;      
+    }
+
+    containsKey(ch) {
+        return this.links[ch] !== undefined;
+    }
+    get(ch) {
+        return this.links[ch];
+    }
+    put(ch, node) {
+        this.links[ch] = node;
+    }
+    setEnd() {
+        this.isEnd = true;
+    }   
+}
 
 var WordDictionary = function() {
-    
+    this.root = new TrieNode();  
 };
 
 /** 
@@ -8,7 +31,42 @@ var WordDictionary = function() {
  * @return {void}
  */
 WordDictionary.prototype.addWord = function(word) {
+    node = this.root;
+    for (let i = 0; i < word.length; i++) {
+        currentChar = word[i];
+        if (!node.containsKey(currentChar)) {
+            node.put(currentChar, new TrieNode());
+        }
+        node = node.get(currentChar);
+    }
+    node.setEnd();
+};
+
+/** 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+ WordDictionary.prototype.searchPrefix = function(prefix) {
+    nodes = [this.root];
     
+    for (let i = 0; i < prefix.length; i++) {
+        let newNodes = [];
+        curLetter = prefix[i];
+        
+        nodes.forEach(node => {
+            
+            if (curLetter === '.'){               
+                Object.keys(node.links).forEach(key => {                   
+                    newNodes.push(node.links[key]);
+                })               
+            } else if (node.containsKey(curLetter)) {
+                newNodes.push(node.get(curLetter));
+            } 
+        })
+        nodes = newNodes;
+        if (!nodes.length) break;
+    }
+    return nodes;
 };
 
 /** 
@@ -16,7 +74,12 @@ WordDictionary.prototype.addWord = function(word) {
  * @return {boolean}
  */
 WordDictionary.prototype.search = function(word) {
-    
+    nodes = this.searchPrefix(word);
+    if (!nodes.length) return false;
+    for(let i = 0; i < nodes.length; i++) {       
+        if (nodes[i].isEnd) return true;
+    }
+    return false;
 };
 
 /** 
