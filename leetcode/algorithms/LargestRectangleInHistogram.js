4f078a0d5d3362403dcd20d9c873c351e8ba9bca
Beatrix_Kido <la_katalina_s@mail.ru>
2022-01-29 12:29:03 +0300

    Largest Rectangle in Histogram

--- leetcode/algorithms/LargestRectangleInHistogram.js
+++ leetcode/algorithms/LargestRectangleInHistogram.js
@@ -5,28 +5,22 @@
  * @param {number[]} heights
  * @return {number}
  */
+//O(n)
  var largestRectangleArea = function(heights) {    
-    let min_val = Math.min(...heights);
-    let max_val = Math.max(...heights);   
+    
     let n = heights.length;
-    let max = min_val * n;     	
-    if (min_val === max_val) return max;            
-    for(let i = 0; i < n; i++) {                        
-        let left = 0;
-        for(let j = i-1; j >=0; j--) {
-            if (heights[j] < heights[i]){
-                left = j + 1;                
-                break;
-            }
+    let max = 0;     	
+    let stack = [];
+          
+    for(let i = 0; i <= n; i++) {  
+        let step = 0;
+        let bar = heights[i] || 0;
+        while(stack.length && stack[stack.length-1][1] >= bar) {
+            let [w, h] = stack.pop();
+            step += w;
+			max = Math.max(max, step * h);
         }
-        let right = n-1;
-        for(let j = i+1; j < n; j++) {
-            if (heights[j] < heights[i]){   
-                right = j - 1;                                
-                break;
-            }                
-        }        
-        max = Math.max(max, (right - left + 1) * heights[i]);                          
+        stack.push([step+1, bar]);                   
     }    
 	return max;	
 };
