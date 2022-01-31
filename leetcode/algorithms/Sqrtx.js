3e34ee48320561b511f05582331731949c15a84c
Beatrix_Kido <la_katalina_s@mail.ru>
2022-01-31 08:06:08 +0300

    Sqrt(x)

---
+++ leetcode/algorithms/Sqrtx.js
@@ -0,0 +1,21 @@
+// Source : https://leetcode.com/problems/sqrtx/
+// Author : avebeatrix
+// Date   : 2022-01-31
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    let l = 0;
+    let r = Math.floor((x + 1) / 2);
+    while (l < r) {
+        let m = Math.floor((l + r + 1) / 2);
+        if (m * m > x) {
+            r = m - 1;
+        } else {
+            l = m;
+        }
+    }
+    return l;
+};
+console.log(mySqrt(125));
