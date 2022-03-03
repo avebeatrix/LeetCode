ac6eb4ac9c0c0f50a9b867ed342c50b6ade685b6
Beatrix_Kido <la_katalina_s@mail.ru>
2022-03-03 12:25:17 +0300

    Arithmetic Slices

---
+++ leetcode/algorithms/ArithmeticSlices.js
@@ -0,0 +1,34 @@
+// Source : https://leetcode.com/problems/arithmetic-slices/
+// Author : avebeatrix
+// Date   : 2022-03-03
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numberOfArithmeticSlices = function (nums) {
+    let result = 0;
+    let l = nums.length;
+    if (l < 3) return result;
+    let diff = nums[1] - nums[0];
+    let cur_l = 2;
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] - nums[i - 1] === diff) {
+            cur_l++;
+        } else {
+            if (cur_l >= 3) {
+                for (let j = cur_l - 2; j > 0; j--) {
+                    result += j;
+                }
+            }
+            cur_l = 2;
+            diff = nums[i] - nums[i - 1];
+        }
+    }
+    if (cur_l >= 3) {
+        for (let j = cur_l - 2; j > 0; j--) {
+            result += j;
+        }
+    }
+    return result;
+};
+console.log(numberOfArithmeticSlices([1,3,5,7,9, 0, 0,0]));
