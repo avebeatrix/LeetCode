f8438032a77d0d4c4d9fb98d89d46e0720d0f99c
Beatrix_Kido <la_katalina_s@mail.ru>
2022-03-23 10:31:45 +0300

    Broken Calculator

---
+++ leetcode/algorithms/BrokenCalculator.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} startValue
+ * @param {number} target
+ * @return {number}
+ */
+var brokenCalc = function (startValue, target) {
+    let res = 0;
+    let cur = target;
+    while (cur > startValue) {       
+        if (cur % 2 === 0) {
+            cur /= 2;
+            res++;
+        } else {
+            cur = (cur + 1) / 2;
+            res += 2;
+        }       
+    }  
+    res += startValue - cur;         
+    return res;
+};
+console.log(brokenCalc(startValue = 6, target = 13));
