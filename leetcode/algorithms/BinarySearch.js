4444095e2a98f0596a6b4751fde028f98c9ea9e3
Beatrix_Kido <la_katalina_s@mail.ru>
2022-03-26 14:43:34 +0300

    Binary Search

---
+++ leetcode/algorithms/BinarySearch.js
@@ -0,0 +1,24 @@
+// Source : https://leetcode.com/problems/binary-search/
+// Author : avebeatrix
+// Date   : 2022-03-26
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var search = function(nums, target) {
+    let l = 0;
+    let r = nums.length;    
+    while (l < r) {
+        let m = Math.floor((l + r) / 2);
+        if (nums[m] >= target) {
+            r = m;         
+        } else {           
+            l = m + 1;    
+        }
+    }
+    if (nums[r] !== target) return -1;
+    return r;   
+};
+//console.log(search(nums = [-1,0,3,5,9,12], target = 9));
+console.log(search(nums = [-1,0,3,5,9], target = 3));
