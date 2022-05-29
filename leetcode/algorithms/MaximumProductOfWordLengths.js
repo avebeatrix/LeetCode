d0b7861389ee0cb68f5f5f9753f544f4e3ea8fe8
Beatrix_Kido <la_katalina_s@mail.ru>
2022-05-29 10:53:41 +0300

    Maximum Product of Word Lengths

---
+++ leetcode/algorithms/MaximumProductOfWordLengths.js
@@ -0,0 +1,27 @@
+// Source : https://leetcode.com/problems/maximum-product-of-word-lengths/
+// Author : avebeatrix
+// Date   : 2022-05-29
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+ var maxProduct = function(words) {
+    let binary_words = [];
+    words.forEach(word => {
+        let binary_word = 0;
+        [...(new Set(word))].forEach(letter => {
+            binary_word |= parseInt('1' + '0'.repeat(letter.charCodeAt(0) - 'a'.charCodeAt(0)), 2);
+        })
+        binary_words.push(binary_word);
+    })
+    let N = words.length;
+    let max = 0;
+    for (let i = 0; i < N; i++) {
+        for (let j = i + 1; j < N; j++) {
+            if ((binary_words[i] | binary_words[j]) === (binary_words[i] ^ binary_words[j])) {
+                max = Math.max(max, words[i].toString().length * words[j].toString().length);
+            }
+        }
+    }
+    return max;
+};
